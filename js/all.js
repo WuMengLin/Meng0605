82ade72a2177d13d32566e32733ce6e504aa6a83
Meng <leo80625@gmail.com>
2017-06-10 20:26:54 +0800

    新增註冊系統

--- js/all.js
+++ js/all.js
@@ -49,3 +49,38 @@ $(document).ready(function() {
 	});
 	$('.meals li:eq(3) .add').click();
 });
+
+var body=document.body;
+var register_mail=document.getElementById('register_mail');
+var register_password=document.getElementById('register_password');
+var register_password_check=document.getElementById('register_password_check');
+var buttom_register=document.querySelector('.buttom_register');
+var xhr=new XMLHttpRequest();
+
+function sendData(e){
+	e.preventDefault();
+	if(register_password.value==register_password_check.value){
+		xhr.open('post','https://hexschool-tutorial.herokuapp.com/api/signup',true);
+		xhr.setRequestHeader('content-type','application/x-www-form-urlencoded');
+		var str ='email='+register_mail.value+'&password='+register_password.value;
+		xhr.send(str);
+		register_password.value='';
+		register_password_check.value='';
+	}else{
+		alert('兩次輸入的密碼不相同')
+		register_password.value='';
+		register_password_check.value='';
+	}
+};
+
+buttom_register.addEventListener('click',sendData,false);
+
+xhr.onload=function(){
+	var array=[];
+	array=JSON.parse(xhr.responseText);
+	alert(array.message);
+}
+
+body.addEventListener('keydown',function(e){
+	if(e.keyCode=='13'){sendData(e);}
+},false);
